9bf73a454ba749fcd480822ded80a8d7a2aa5454
falcu2345 <falcu2345@gmail.com>
2016-10-08 18:59:26 -0500

    0810

--- src/BLL/CargosBLL.java
+++ src/BLL/CargosBLL.java
@@ -4,17 +4,29 @@ package BLL;
 import DAL.IEmpresa;
 import ENTITIES.Cargos;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class CargosBLL implements IEmpresa<Cargos> {
-
+    
+    ArrayList<Cargos> lsCargos = new ArrayList<>();
+    
     @Override
     public void guardar(Cargos obj) {
+        
+        try {
+            
+            lsCargos.add(obj);
+            
+        } catch (Exception e) {
+            throw e;
+        }
         
     }
 
     @Override
     public void eliminar(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not suppor"
+                + "ted yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -24,7 +36,13 @@ public class CargosBLL implements IEmpresa<Cargos> {
 
     @Override
     public Cargos buscarId(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        try {
+            return lsCargos.stream().filter(r -> r.getId() == (Integer)id).findFirst().orElse(null);        
+        } catch (Exception ex) {
+            throw ex;
+        }
+        
     }
 
     @Override
